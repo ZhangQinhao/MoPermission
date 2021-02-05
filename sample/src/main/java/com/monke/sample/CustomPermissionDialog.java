c6755be2f6a3606c6598cdc9a4eeeff569e89681
zhangqinhao <1105075896@qq.com>
2021-02-06 01:47:26 +0800

    1

---
+++ sample/src/main/java/com/monke/sample/CustomPermissionDialog.java
@@ -0,0 +1,98 @@
+package com.monke.sample;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.monke.mopermission.MoPermissionBaseDialog;
+
+public class CustomPermissionDialog extends MoPermissionBaseDialog {
+    private TextView tvTitle;
+    private TextView tvDesc;
+    private Button tvNo;
+    private Button tvYes;
+
+    public CustomPermissionDialog(Context context) {
+        super(context);
+        setContentView(R.layout.layout_confirmdialog);
+        tvTitle = findViewById(com.monke.mopermission.R.id.tv_title);
+        tvDesc = findViewById(com.monke.mopermission.R.id.tv_desc);
+        tvNo = findViewById(com.monke.mopermission.R.id.tv_no);
+        tvYes = findViewById(com.monke.mopermission.R.id.tv_yes);
+
+        tvTitle.setText(titleStr);
+        tvDesc.setText(descStr);
+        if (TextUtils.isEmpty(descStr)) {
+            tvDesc.setVisibility(View.GONE);
+        } else {
+            tvDesc.setVisibility(View.VISIBLE);
+        }
+        tvYes.setText(yesStr);
+        tvNo.setText(noStr);
+
+        tvNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickToCancel(v);
+            }
+        });
+        tvYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickToRequest(v);
+            }
+        });
+
+        setCancelable(false);
+
+        Window dialogWindow = getWindow();
+        dialogWindow.setDimAmount(0.6f);
+        dialogWindow.getDecorView().setBackgroundResource(com.monke.mopermission.R.color.transparent);
+        dialogWindow.getDecorView().setPadding(0, 0, 0, 0);
+        dialogWindow.setGravity(Gravity.CENTER);
+
+        WindowManager.LayoutParams p = dialogWindow.getAttributes();
+        if (p != null) {
+            p.width = WindowManager.LayoutParams.WRAP_CONTENT;
+            p.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            p.gravity = Gravity.CENTER;
+            dialogWindow.setAttributes(p);
+        }
+    }
+
+    private String titleStr;
+    private String descStr;
+    private String yesStr;
+    private String noStr;
+
+    @Override
+    public void show(String titleStr, String descStr, String yesStr, String noStr, View.OnClickListener yesClickListener, View.OnClickListener noClickListener) {
+        super.show(titleStr, descStr, yesStr, noStr, yesClickListener, noClickListener);
+        this.titleStr = titleStr;
+        this.descStr = descStr;
+        this.yesStr = yesStr;
+        this.noStr = noStr;
+        if (tvTitle != null) {
+            tvTitle.setText(titleStr);
+        }
+        if (tvDesc != null) {
+            tvDesc.setText(descStr);
+            if (TextUtils.isEmpty(descStr)) {
+                tvDesc.setVisibility(View.GONE);
+            } else {
+                tvDesc.setVisibility(View.VISIBLE);
+            }
+        }
+        if (tvYes != null) {
+            tvYes.setText(yesStr);
+        }
+        if (tvNo != null) {
+            tvNo.setText(noStr);
+        }
+    }
+}
---
+++ sample/src/main/java/com/monke/sample/MainActivity.java
@@ -0,0 +1,242 @@
+package com.monke.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.monke.mopermission.MoPermission;
+import com.monke.mopermission.OnRequestNecessaryPermissionListener;
+import com.monke.mopermission.OnRequestPermissionListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //网络权限测试
+        initNet();
+        //手机信息权限测试
+        initPhone();
+        //手机信息和SD卡读写权限测试
+        initSDandPhone();
+        //sd卡麦克风悬浮窗权限测试
+        initSDMICWINDOW();
+        //摄像头与发送短信权限测试(自定义二次确认弹窗UI)
+        initCameraMSG();
+    }
+
+    private void initCameraMSG() {
+        findViewById(R.id.tv_5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoPermission.Companion.requestPermission(MainActivity.this, new OnRequestPermissionListener() {
+                    @Override
+                    public void requestPermission(List<String> permissions) {
+                        if (permissions.size() >= 2) {
+                            showToast("已获取摄像头,短信发送权限");
+                        } else {
+                            if (permissions.size() == 0) {
+                                showToast("未获取摄像头,短信发送权限");
+                            } else {
+                                String temp = "已获取";
+                                if (permissions.contains(Manifest.permission.CAMERA)) {
+                                    temp += "摄像头";
+                                }
+                                if (permissions.contains(Manifest.permission.SEND_SMS)) {
+                                    temp += "短信发送";
+                                }
+                                temp += "权限";
+                                showToast(temp);
+                            }
+                        }
+                    }
+                }, CustomPermissionDialog.class, Manifest.permission.CAMERA, Manifest.permission.SEND_SMS);
+            }
+        });
+        findViewById(R.id.tv_5_necessary).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoPermission.Companion.requestNecessaryPermission(MainActivity.this, "权限申请", "获取摄像头与短信发送权限", "申请", "退出", new OnRequestNecessaryPermissionListener() {
+                    @Override
+                    public void success(List<String> permissions) {
+                        showToast("已获取读取SD卡,麦克风,悬浮窗权限");
+                    }
+
+                    @Override
+                    public void fail(List<String> permissions) {
+                        showToast("未获取读取SD卡,麦克风,悬浮窗权限！可以关闭相关功能");
+                    }
+                }, CustomPermissionDialog.class, Manifest.permission.CAMERA, Manifest.permission.SEND_SMS);
+            }
+        });
+    }
+
+    private void initSDMICWINDOW() {
+        findViewById(R.id.tv_4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoPermission.Companion.requestPermission(MainActivity.this, new OnRequestPermissionListener() {
+                    @Override
+                    public void requestPermission(List<String> permissions) {
+                        if (permissions.size() >= 3) {
+                            showToast("已获取读取SD卡,麦克风,悬浮窗权限");
+                        } else {
+                            if (permissions.size() == 0) {
+                                showToast("未获取读取SD卡,麦克风,悬浮窗权限");
+                            } else {
+                                String temp = "已获取";
+                                if (permissions.contains(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                                    temp += "SD卡";
+                                }
+                                if (permissions.contains(Manifest.permission.RECORD_AUDIO)) {
+                                    temp += "麦克风";
+                                }
+                                if (permissions.contains(Manifest.permission.SYSTEM_ALERT_WINDOW)) {
+                                    temp += "悬浮窗";
+                                }
+                                temp += "权限";
+                                showToast(temp);
+                            }
+                        }
+                    }
+                }, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW);
+            }
+        });
+        findViewById(R.id.tv_4_necessary).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoPermission.Companion.requestNecessaryPermission(MainActivity.this, "获取手机信息和SD卡权限和悬浮窗权限", new OnRequestNecessaryPermissionListener() {
+                    @Override
+                    public void success(List<String> permissions) {
+                        showToast("已获取读取SD卡,麦克风,悬浮窗权限");
+                    }
+
+                    @Override
+                    public void fail(List<String> permissions) {
+                        showToast("未获取读取SD卡,麦克风,悬浮窗权限！可以关闭相关功能");
+                    }
+                }, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW);
+            }
+        });
+    }
+
+    private void initSDandPhone() {
+        findViewById(R.id.tv_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoPermission.Companion.requestPermission(MainActivity.this, new OnRequestPermissionListener() {
+                    @Override
+                    public void requestPermission(List<String> permissions) {
+                        if (permissions.size() >= 2) {
+                            showToast("已获取读取手机信息权限和SD卡权限");
+                        } else {
+                            if (permissions.size() == 0) {
+                                showToast("未获取手机信息权限和SD卡权限");
+                            } else {
+                                if (permissions.contains(Manifest.permission.READ_PHONE_STATE)) {
+                                    showToast("未获取SD卡权限");
+                                } else {
+                                    showToast("未获取手机信息权限");
+                                }
+                            }
+                        }
+                    }
+                }, Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+        });
+        findViewById(R.id.tv_3_necessary).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoPermission.Companion.requestNecessaryPermission(MainActivity.this, "获取手机信息和SD卡权限", new OnRequestNecessaryPermissionListener() {
+                    @Override
+                    public void success(List<String> permissions) {
+                        showToast("已获取读取手机信息权限和SD卡权限");
+                    }
+
+                    @Override
+                    public void fail(List<String> permissions) {
+                        showToast("未获取读取手机信息权限和SD卡权限！可以关闭相关功能");
+                    }
+                }, Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+        });
+    }
+
+    private void initPhone() {
+        findViewById(R.id.tv_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoPermission.Companion.requestPermission(MainActivity.this, new OnRequestPermissionListener() {
+                    @Override
+                    public void requestPermission(List<String> permissions) {
+                        if (permissions.size() >= 1) {
+                            showToast("已获取读取手机信息权限");
+                        } else {
+                            showToast("未获取读取手机信息权限！");
+                        }
+                    }
+                }, Manifest.permission.READ_PHONE_STATE);
+            }
+        });
+        findViewById(R.id.tv_2_necessary).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoPermission.Companion.requestNecessaryPermission(MainActivity.this, "获取读取手机信息权限", new OnRequestNecessaryPermissionListener() {
+                    @Override
+                    public void success(List<String> permissions) {
+                        showToast("已获取读取手机信息权限");
+                    }
+
+                    @Override
+                    public void fail(List<String> permissions) {
+                        showToast("未获取读取手机信息权限！可以关闭相关功能");
+                    }
+                }, Manifest.permission.READ_PHONE_STATE);
+            }
+        });
+    }
+
+    private void initNet() {
+        findViewById(R.id.tv_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoPermission.Companion.requestPermission(MainActivity.this, new OnRequestPermissionListener() {
+                    @Override
+                    public void requestPermission(List<String> permissions) {
+                        if (permissions.size() >= 1) {
+                            showToast("已获取网络权限");
+                        } else {
+                            showToast("未获取网络权限！");
+                        }
+                    }
+                }, Manifest.permission.INTERNET);
+            }
+        });
+        findViewById(R.id.tv_1_necessary).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoPermission.Companion.requestNecessaryPermission(MainActivity.this, "获取网络权限", new OnRequestNecessaryPermissionListener() {
+                    @Override
+                    public void success(List<String> permissions) {
+                        showToast("已获取网络权限");
+                    }
+
+                    @Override
+                    public void fail(List<String> permissions) {
+                        showToast("未获取网络权限！可以关闭相关功能");
+                    }
+                }, Manifest.permission.INTERNET);
+            }
+        });
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
