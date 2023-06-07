81ffe0290c6ca0eb42fb2bd79a8e573a7b23ddfd
monke <1105075896@qq.com>
2023-06-07 20:42:12 +0800

    更新

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # MoPermission
+最方便的安卓权限申请框架
+  
+### 引入
+
+在build.gradle引入  `implementation 'io.github.zhangqinhao:MoPermission:1.0.1'`
+
+### 使用
+![enter description here][1]
+
+#### 非必要权限获取（默认申请权限，如果用户选取取消且不再提示，则不做兜底操作）
+``` stylus
+MoPermission.Companion.requestPermission(MainActivity.this, new OnRequestPermissionListener() {
+                    @Override
+                    public void requestPermission(List<String> permissions) {
+                        if (permissions.size() >= 2) {
+                            showToast("已获取摄像头,短信发送权限");
+                        } else {
+                            if (permissions.size() == 0) {
+                                showToast("未获取摄像头,短信发送权限");
+                            } else {
+                                String temp = "已获取";
+                                if (permissions.contains(Manifest.permission.CAMERA)) {
+                                    temp += "摄像头";
+                                }
+                                if (permissions.contains(Manifest.permission.SEND_SMS)) {
+                                    temp += "短信发送";
+                                }
+                                temp += "权限";
+                                showToast(temp);
+                            }
+                        }
+                    }
+                }, CustomPermissionDialog.class, Manifest.permission.CAMERA, Manifest.permission.SEND_SMS);
+```
+
+
+#### 申请必要权限 (如果用户未获取相关权限则弹窗提示再次申请，如果用户选取取消且不再提示，则手动申请后跳转到系统权限设置页)
+``` stylus
+MoPermission.Companion.requestNecessaryPermission(MainActivity.this, "权限申请", "获取摄像头与短信发送权限", "申请", "退出", new OnRequestNecessaryPermissionListener() {
+                    @Override
+                    public void success(List<String> permissions) {
+                        showToast("已获取读取SD卡,麦克风,悬浮窗权限");
+                    }
+
+                    @Override
+                    public void fail(List<String> permissions) {
+                        showToast("未获取读取SD卡,麦克风,悬浮窗权限！可以关闭相关功能");
+                    }
+                }, CustomPermissionDialog.class, Manifest.permission.CAMERA, Manifest.permission.SEND_SMS);
+```
+
+
+#### 弹窗样式自定义修改
+继承实现 MoPermissionBaseDialog 相关方法并定制UI，可参考MoPermissionDialog或者Sample中CustomPermissionDialog  
+注意：必须实现show(String titleStr, String descStr, String yesStr, String noStr, View.OnClickListener yesClickListener, View.OnClickListener noClickListener)  
+申请的点击事件需要执行clickToRequest(),取消/退出的点击事件需要执行clickToCancel()
+
+
+具体用法参照Sample代码
+
+[1]: ./images/1.gif "1.gif"
--- app/src/main/java/com/monke/mopermission/MoPermission.kt
+++ app/src/main/java/com/monke/mopermission/MoPermission.kt
@@ -2,7 +2,6 @@ package com.monke.mopermission
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
@@ -14,6 +13,7 @@ open class MoPermission {
 
     companion object {
         private var sdkInt: Int = -1
+
         fun getSdkInt(): Int {
             if (sdkInt > 0) {
                 return sdkInt
@@ -25,52 +25,138 @@ open class MoPermission {
         /**
          * 申请普通权限
          */
-        fun requestPermission(context: Context, listener: OnRequestPermissionListener, vararg permission: String) {
-            requestPermission(context, listener, null, *permission)
+        @JvmStatic
+        fun requestPermission(
+            context: Context,
+            title: String,
+            yesStr: String,
+            noStr: String?,
+            listener: OnRequestPermissionListener,
+            vararg permission: String
+        ) {
+            requestPermission(context, title, null, yesStr, noStr, listener, null, *permission)
         }
 
-        fun requestPermission(context: Context, listener: OnRequestPermissionListener, uiClass: Class<out MoPermissionBaseDialog>?, vararg permission: String) {
-            requestPermission(context, null, null, null, null, listener, uiClass, *permission)
-        }
-
-        fun requestPermission(context: Context, title: String?, desc: String?, yesStr: String?, noStr: String?, listener: OnRequestPermissionListener, uiClass: Class<out MoPermissionBaseDialog>?, vararg permission: String) {
-            if (getSdkInt() < Build.VERSION_CODES.M || permission.isEmpty() || checkPermissions(context, *permission)) {
+        @JvmStatic
+        fun requestPermission(
+            context: Context,
+            title: String?,
+            desc: String?,
+            yesStr: String?,
+            noStr: String?,
+            listener: OnRequestPermissionListener,
+            uiClass: Class<out MoPermissionBaseDialog>?,
+            vararg permission: String
+        ) {
+            if (getSdkInt() < Build.VERSION_CODES.M || permission.isEmpty() || checkPermissions(
+                    context,
+                    *permission
+                )
+            ) {
                 listener.requestPermission(permission.asList())
             } else {
-                requestPermissionActivity(context, false, title, desc, yesStr, noStr, PermissionJustOnceObserver(context, listener), uiClass, *permission)
+                requestPermissionActivity(
+                    context,
+                    false,
+                    title,
+                    desc,
+                    yesStr,
+                    noStr,
+                    PermissionJustOnceObserver(context, listener),
+                    uiClass,
+                    *permission
+                )
             }
         }
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         /**
          * 申请必要权限
          */
-        fun requestNecessaryPermission(context: Context, title: String?, listener: OnRequestNecessaryPermissionListener, vararg permission: String) {
-            requestNecessaryPermission(context, title, null, null, null, listener, *permission)
+        @JvmStatic
+        fun requestNecessaryPermission(
+            context: Context,
+            title: String,
+            yesStr: String,
+            noStr: String?,
+            listener: OnRequestNecessaryPermissionListener,
+            vararg permission: String
+        ) {
+            requestNecessaryPermission(
+                context,
+                title,
+                null,
+                yesStr,
+                noStr,
+                listener,
+                null,
+                *permission
+            )
         }
 
-        fun requestNecessaryPermission(context: Context, title: String?, desc: String?, yesStr: String?, noStr: String?, listener: OnRequestNecessaryPermissionListener, vararg permission: String) {
-            requestNecessaryPermission(context, title, desc, yesStr, noStr, listener, null, *permission)
-        }
-
-        fun requestNecessaryPermission(context: Context, title: String?, desc: String?, yesStr: String?, noStr: String?, listener: OnRequestNecessaryPermissionListener, uiClass: Class<out MoPermissionBaseDialog>?, vararg permission: String) {
-            if (getSdkInt() < Build.VERSION_CODES.M || permission.isEmpty() || checkPermissions(context, *permission)) {
+        @JvmStatic
+        fun requestNecessaryPermission(
+            context: Context,
+            title: String,
+            desc: String?,
+            yesStr: String?,
+            noStr: String?,
+            listener: OnRequestNecessaryPermissionListener,
+            uiClass: Class<out MoPermissionBaseDialog>?,
+            vararg permission: String
+        ) {
+            if (getSdkInt() < Build.VERSION_CODES.M || permission.isEmpty() || checkPermissions(
+                    context,
+                    *permission
+                )
+            ) {
                 listener.success(permission.asList())
             } else {
-                requestPermissionActivity(context, true, title, desc, yesStr, noStr, NecessaryPermissionJustOnceObserver(context, listener, permission.size), uiClass, *permission)
+                requestPermissionActivity(
+                    context,
+                    true,
+                    title,
+                    desc,
+                    yesStr,
+                    noStr,
+                    NecessaryPermissionJustOnceObserver(context, listener, permission.size),
+                    uiClass,
+                    *permission,
+                )
             }
         }
 
         ////////////////////////////////////////////////////////////////////////////////////////////
 
-        private fun requestPermissionActivity(context: Context, necessary: Boolean, title: String?, desc: String?, yesStr: String?, noStr: String?, observer: Observer<List<String>>, uiClass: Class<out MoPermissionBaseDialog>?, vararg permission: String) {
+        private fun requestPermissionActivity(
+            context: Context,
+            necessary: Boolean,
+            title: String?,
+            desc: String?,
+            yesStr: String?,
+            noStr: String?,
+            observer: Observer<List<String>>,
+            uiClass: Class<out MoPermissionBaseDialog>?,
+            vararg permission: String
+        ) {
             //启动权限获取页面
             val key: String = UUID.randomUUID().toString() + System.currentTimeMillis()
             MoPermissionBus.getInstance()!!.registerJustReportOnce(key, observer)
-            MoPermissionActivity.start(context, necessary, key, title, desc, yesStr, noStr, uiClass, *permission)
+            MoPermissionActivity.start(
+                context,
+                necessary,
+                key,
+                title,
+                desc,
+                yesStr,
+                noStr,
+                uiClass,
+                *permission
+            )
         }
 
         ////////////////////////////////////////////////////////////////////////////////////////////
 
+        @JvmStatic
         @SuppressLint("NewApi")
         fun checkPermissions(context: Context, vararg permission: String): Boolean {
             if (permission.isEmpty()) {
@@ -88,6 +174,7 @@ open class MoPermission {
             return true
         }
 
+        @JvmStatic
         @SuppressLint("NewApi")
         fun getNoPermissionLimitOne(context: Context, vararg permission: String): String? {
             if (permission.isEmpty()) {
@@ -105,6 +192,7 @@ open class MoPermission {
             return null
         }
 
+        @JvmStatic
         fun checkPermission(context: Context, permission: String): Boolean {
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
                 return true
@@ -113,9 +201,11 @@ open class MoPermission {
                     Manifest.permission.WRITE_SETTINGS -> {
                         Settings.System.canWrite(context)
                     }
+
                     Manifest.permission.SYSTEM_ALERT_WINDOW -> {
                         Settings.canDrawOverlays(context)
                     }
+
                     else -> {
                         val result = context.checkSelfPermission(permission)
                         result == PackageManager.PERMISSION_GRANTED
